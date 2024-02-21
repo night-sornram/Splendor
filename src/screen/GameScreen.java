ba91c54ea86c805c3624ec4f0ca8882f6536a4d0
Sornram Chareanma <sornram.chalernma@gmail.com>
2024-02-21 21:06:47 +0700

    GameScreen can split menu

--- src/screen/GameScreen.java
+++ src/screen/GameScreen.java
@@ -1,46 +1,63 @@
 package screen;
 
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.SplitPane;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import utils.Property;
 
 public class GameScreen extends VBox {
     private Stage stage;
     private static StackPane root;
-    private Canvas canvas;
-    private GraphicsContext gc;
     public GameScreen(Stage stage, int numberOfPlayer){
         this.stage = stage;
-        VBox root = new VBox();
-        Scene scene = new Scene(root);
-        this.stage.setScene(scene);
-        canvas = new Canvas(Property.WIDTH,Property.HEIGHT);
-        gc = canvas.getGraphicsContext2D();
-        System.out.println(Property.NUMBER_OF_PLAYER);
-        setUp();
 
-    }
-    public void draw(GraphicsContext gc) {
-        root = new StackPane();
-        root.setPrefSize(Property.WIDTH,Property.HEIGHT);
-        root.getChildren().addAll(canvas);
+        //Create left-hand titled pane for the films list and centre it in VBox
+        Text films = new Text();
+        films.setText("Films");
+        Pane test1 = new Pane();
+        test1.setPrefWidth(1200);
+        test1.setPrefHeight(500);
+        test1.setBackground(new Background(new BackgroundFill(Color.RED, new CornerRadii(0), new Insets(0))));
+        VBox filmsBox = new VBox(test1);
+
+        //Create right-hand titled pane for the books list and centre it in Vbox
+        Text books = new Text();
+        books.setText("Books");
+        VBox booksBox = new VBox(books);
+
+        //Create mainSplit pane
+        Text arts = new Text();
+        arts.setText("arts");
+        VBox artsBox = new VBox(arts);
+
+        SplitPane mainSplit = new SplitPane();
+        mainSplit.getItems().addAll(filmsBox,  booksBox);
+        mainSplit.setDividerPosition(0,10/(double)12);
+
+        SplitPane root = new SplitPane();
+        root.setOrientation(Orientation.VERTICAL);
+        root.getItems().addAll(mainSplit, artsBox);
+        root.setDividerPosition(0,0.7);
+
+        root.setPrefWidth(1280);
+
+        root.setPrefHeight(720);
+        SplitPane.setResizableWithParent(root, false);
+        //Start up scene and stage
         Scene scene = new Scene(root);
         stage.setScene(scene);
-    }
-    public void setUp() {
-        Image image = new Image(ClassLoader.getSystemResource("picture/test.jpg").toString());
-        gc.drawImage(image,0, 0,Property.WIDTH,Property.HEIGHT);
-        gc.setFill(Color.BLACK);
-        draw(gc);
-    }
 
+    }
 
 }
