7deba7263d93fa1f06a061dee9dafe26aa7c59be
Sornram Chareanma <sornram.chalernma@gmail.com>
2024-02-21 21:16:53 +0700

    GameScrenn can not resize

--- src/screen/GameScreen.java
+++ src/screen/GameScreen.java
@@ -21,42 +21,26 @@ public class GameScreen extends VBox {
     private static StackPane root;
     public GameScreen(Stage stage, int numberOfPlayer){
         this.stage = stage;
+        VBox root = new VBox();
+        Pane p1 = new Pane();
+        p1.setPrefSize(1000,500);
+        Pane p2 = new Pane();
+        p2.setPrefSize(280,500);
+        p1.setBackground(new Background(new BackgroundFill(Color.RED, new CornerRadii(0), new Insets(0))));
+        p2.setBackground(new Background(new BackgroundFill(Color.BLUE, new CornerRadii(0), new Insets(0))));
+        HBox menu = new HBox();
+        menu.getChildren().addAll(p1,p2);
+        Pane Control = new Pane();
+        Control.setBackground(new Background(new BackgroundFill(Color.GREEN, new CornerRadii(0), new Insets(0))));
+        root.setPrefSize(Property.WIDTH,Property.HEIGHT);
+        menu.setPrefSize(1280,500);
+        Control.setPrefSize(1280,220);
 
-        //Create left-hand titled pane for the films list and centre it in VBox
-        Text films = new Text();
-        films.setText("Films");
-        Pane test1 = new Pane();
-        test1.setPrefWidth(1200);
-        test1.setPrefHeight(500);
-        test1.setBackground(new Background(new BackgroundFill(Color.RED, new CornerRadii(0), new Insets(0))));
-        VBox filmsBox = new VBox(test1);
-
-        //Create right-hand titled pane for the books list and centre it in Vbox
-        Text books = new Text();
-        books.setText("Books");
-        VBox booksBox = new VBox(books);
-
-        //Create mainSplit pane
-        Text arts = new Text();
-        arts.setText("arts");
-        VBox artsBox = new VBox(arts);
-
-        SplitPane mainSplit = new SplitPane();
-        mainSplit.getItems().addAll(filmsBox,  booksBox);
-        mainSplit.setDividerPosition(0,10/(double)12);
-
-        SplitPane root = new SplitPane();
-        root.setOrientation(Orientation.VERTICAL);
-        root.getItems().addAll(mainSplit, artsBox);
-        root.setDividerPosition(0,0.7);
-
-        root.setPrefWidth(1280);
-
-        root.setPrefHeight(720);
-        SplitPane.setResizableWithParent(root, false);
-        //Start up scene and stage
+        root.getChildren().addAll(menu,Control);
         Scene scene = new Scene(root);
         stage.setScene(scene);
+
+
 
     }
 
